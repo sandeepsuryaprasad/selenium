0c782cddf104badcd83480a703366a8bcbe44ce4
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-06-17 16:14:53 +0530

    [py] added type hints common/action_chains.py, alert.py and support/relative_locator.py

--- py/selenium/webdriver/support/relative_locator.py
+++ py/selenium/webdriver/support/relative_locator.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from typing import TYPE_CHECKING
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -23,36 +24,8 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
-
-def with_tag_name(tag_name: str) -> "RelativeBy":
-    """Start searching for relative objects using a tag name.
-
-    Note: This method may be removed in future versions, please use
-    `locate_with` instead.
-    :Args:
-        - tag_name: the DOM tag of element to start searching.
-    :Returns:
-        - RelativeBy - use this object to create filters within a
-            `find_elements` call.
-    """
-    if not tag_name:
-        raise WebDriverException("tag_name can not be null")
-    return RelativeBy({"css selector": tag_name})
-
-
-def locate_with(by: By, using: str) -> "RelativeBy":
-    """Start searching for relative objects your search criteria with By.
-
-    :Args:
-        - by: The value from `By` passed in.
-        - using: search term to find the element with.
-    :Returns:
-        - RelativeBy - use this object to create filters within a
-            `find_elements` call.
-    """
-    assert by is not None, "Please pass in a by argument"
-    assert using is not None, "Please pass in a using argument"
-    return RelativeBy({by: using})
+if TYPE_CHECKING:
+    from typing import Self
 
 
 class RelativeBy:
@@ -70,7 +43,7 @@ class RelativeBy:
         assert "mid" in ids
     """
 
-    def __init__(self, root: Optional[Dict[Union[By, str], str]] = None, filters: Optional[List] = None):
+    def __init__(self, root: Optional[Dict[Union[By, str], str]] = None, filters: Optional[List] = None) -> None:
         """
         Creates a new RelativeBy object. It is preferred if you use the
         `locate_with` method as this signature could change.
@@ -82,7 +55,7 @@ class RelativeBy:
         self.root = root
         self.filters = filters or []
 
-    def above(self, element_or_locator: Union[WebElement, Dict] = None) -> "RelativeBy":
+    def above(self, element_or_locator: Union[WebElement, Dict] = None) -> Self:
         """
         Add a filter to look for elements above.
         :Args:
@@ -94,7 +67,7 @@ class RelativeBy:
         self.filters.append({"kind": "above", "args": [element_or_locator]})
         return self
 
-    def below(self, element_or_locator: Union[WebElement, Dict] = None) -> "RelativeBy":
+    def below(self, element_or_locator: Union[WebElement, Dict] = None) -> Self:
         """
         Add a filter to look for elements below.
         :Args:
@@ -106,7 +79,7 @@ class RelativeBy:
         self.filters.append({"kind": "below", "args": [element_or_locator]})
         return self
 
-    def to_left_of(self, element_or_locator: Union[WebElement, Dict] = None) -> "RelativeBy":
+    def to_left_of(self, element_or_locator: Union[WebElement, Dict] = None) -> Self:
         """
         Add a filter to look for elements to the left of.
         :Args:
@@ -118,7 +91,7 @@ class RelativeBy:
         self.filters.append({"kind": "left", "args": [element_or_locator]})
         return self
 
-    def to_right_of(self, element_or_locator: Union[WebElement, Dict] = None) -> "RelativeBy":
+    def to_right_of(self, element_or_locator: Union[WebElement, Dict] = None) -> Self:
         """
         Add a filter to look for elements right of.
         :Args:
@@ -130,7 +103,7 @@ class RelativeBy:
         self.filters.append({"kind": "right", "args": [element_or_locator]})
         return self
 
-    def near(self, element_or_locator_distance: Union[WebElement, Dict, int] = None) -> "RelativeBy":
+    def near(self, element_or_locator_distance: Union[WebElement, Dict, int] = None) -> Self:
         """
         Add a filter to look for elements near.
         :Args:
@@ -151,3 +124,34 @@ class RelativeBy:
                 "filters": self.filters,
             }
         }
+
+
+def with_tag_name(tag_name: str) -> RelativeBy:
+    """Start searching for relative objects using a tag name.
+
+    Note: This method may be removed in future versions, please use
+    `locate_with` instead.
+    :Args:
+        - tag_name: the DOM tag of element to start searching.
+    :Returns:
+        - RelativeBy - use this object to create filters within a
+            `find_elements` call.
+    """
+    if not tag_name:
+        raise WebDriverException("tag_name can not be null")
+    return RelativeBy({"css selector": tag_name})
+
+
+def locate_with(by: By, using: str) -> RelativeBy:
+    """Start searching for relative objects your search criteria with By.
+
+    :Args:
+        - by: The value from `By` passed in.
+        - using: search term to find the element with.
+    :Returns:
+        - RelativeBy - use this object to create filters within a
+            `find_elements` call.
+    """
+    assert by is not None, "Please pass in a by argument"
+    assert using is not None, "Please pass in a using argument"
+    return RelativeBy({by: using})
